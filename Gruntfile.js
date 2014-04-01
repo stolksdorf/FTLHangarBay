e4ea97b65b1c08c9d46d5332d94f889c91159590
Scott <scott.tolksdorf@gmail.com>
2014-04-01 00:48:41 -0400

    Grunt watch is now setup

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		less: {
+			all: {
+				src: './public/index.less',
+				dest: './public/static/style.css'
+			}
+		},
+		watch: {
+			files: "**/*.less",
+			tasks: ["less"]
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default','watch');
+
+};
