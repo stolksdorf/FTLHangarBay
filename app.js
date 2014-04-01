5fd6660330aebd7f74141ecc963cebf71cb3d335
Scott <scott.tolksdorf@gmail.com>
2014-03-31 23:57:23 -0400

    basic structure built

---
+++ app.js
@@ -0,0 +1,29 @@
+//Globals
+GLOBAL.fs      = require('fs');
+GLOBAL._       = require('underscore');
+GLOBAL.express = express = require("express");
+GLOBAL.app     = express();
+GLOBAL.ejs     = require('ejs');
+
+
+//Express
+app.engine('html', ejs.renderFile);
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 8000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
+
+
+
+app.get('/', function(req, res){
+
+
+	res.render('index.html');
+});
+
+app.get('*', function(req, res){
+	res.render('oops.html');
+})
